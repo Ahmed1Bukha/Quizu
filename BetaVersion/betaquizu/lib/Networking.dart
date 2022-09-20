c5f081ae96e6d30ea9d16ab6dc3a26ab665ec42f
AhmedBukha <xxbu7aamedxx@gmail.com>
2022-09-20 22:30:14 +0300

    Added some networking functionality

--- BetaVersion/betaquizu/lib/Networking.dart
+++ BetaVersion/betaquizu/lib/Networking.dart
@@ -3,13 +3,24 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-const loginInPage = 'quizu.okoul.com';
+const loginInPage = 'quizu.okoul.com/Login';
+const tokenUrl = 'https://quizu.okoul.com/Token';
+
+const tokenForMe =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjMyLCJpYXQiOjE2NjM2OTE3OTl9.cbU8i8a3ywhB1PVkygbSQBFnTjWBUTchxBWPy3A1uwE";
 
 class Networking {
   Future<void> getOTP(String otpNumber, String mobileNumber) async {
-    var url = Uri.https(loginInPage, '/Login');
+    var url = Uri.http(loginInPage);
     var response =
         await http.post(url, body: {"OTP": otpNumber, "mobile": mobileNumber});
+    var decodedResponse = jsonDecode(response.body);
+    print(decodedResponse);
+  }
+
+  Future<void> verifyTokken(String tokken) async {
+    var url = Uri.http(tokenUrl);
+    var response = await http.post(url, headers: {"AUTHORIZATION": tokken});
     var decodedResponse = jsonDecode(response.body);
     print(decodedResponse);
   }
