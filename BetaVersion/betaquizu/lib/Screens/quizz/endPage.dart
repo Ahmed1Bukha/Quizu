6075c5b4f300667f10009e5877327eb74f802159
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2022-09-27 12:50:00 +0300

    Added something
    just imported the svg file and added a pic to test

--- BetaVersion/betaquizu/lib/Screens/quizz/endPage.dart
+++ BetaVersion/betaquizu/lib/Screens/quizz/endPage.dart
@@ -1,6 +1,7 @@
 import 'package:betaquizu/Screens/mainPages/welcomePage.dart';
 import 'package:betaquizu/Screens/quizz/quizPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../../classes/Networking.dart';
 
 class endPage extends StatelessWidget {
@@ -8,44 +9,50 @@ class endPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Lose page"),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage("Images/Untitled-1.png"),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: TextButton(
-              onPressed: () async {
-                final infoUser = await Networking.userInfoGetter();
-                final questions = await Networking.quistionGetter();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => quizPage(),
-                  ),
-                );
-              },
-              child: Text("Reset quiz"),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("Images/undraw_cancel_re_pkdm.svg",
+                semanticsLabel: 'Acme Logo'),
+            Center(
+              child: TextButton(
+                onPressed: () async {
+                  final infoUser = await Networking.userInfoGetter();
+                  final questions = await Networking.quistionGetter();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => quizPage(),
+                    ),
+                  );
+                },
+                child: Text("Reset quiz"),
+              ),
             ),
-          ),
-          Center(
-            child: TextButton(
-              onPressed: () async {
-                final infoUser = await Networking.userInfoGetter();
-                final questions = await Networking.quistionGetter();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => WelcomePage(),
-                  ),
-                );
-              },
-              child: Text("Return to main page."),
-            ),
-          )
-        ],
+            Center(
+              child: TextButton(
+                onPressed: () async {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WelcomePage(),
+                    ),
+                  );
+                },
+                child: Text("Return to main page."),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
